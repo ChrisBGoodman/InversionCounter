fef5854d34593943444f93ab5b9bccaf7fca8928
Christian Goodman <chris.brycers@gmail.com>
2015-02-10 19:16:36 -0600

    Added functions. reOrder not working

--- InversionCounter.java
+++ InversionCounter.java
@@ -24,12 +24,13 @@ public class InversionCounter {
  * 
  * 
  */
-	public int[] source1 = new int[10000];			//Vars to hold all source data 
-	public int[] source2 = new int[10000];
-	public int[] source3 = new int[10000];
-	public int[] source4 = new int[10000];
-	public int[] source5 = new int[10000];
-	public int[] sum = new int[10000];
+	public int[][] source1 = new int[10000][2];			//Vars to hold all source data 
+	public int[][] source2 = new int[10000][2];
+	public int[][] source3 = new int[10000][2];
+	public int[][] source4 = new int[10000][2];
+	public int[][] source5 = new int[10000][2];
+	public int[][] sum = new int[10000][2];
+	public int[][] sortedSum = new int[10000][2];
 	public int[] count = new int[10000];
 	public int inversionCount = 0;
 	
@@ -40,41 +41,53 @@ public class InversionCounter {
 			do{
 				Scanner scanner = new Scanner(new File("src/sources1/source" + fileNum + ".txt"));
 				int i = 0; //reset I 
+				int x = 1; //reset X
 
 				switch (fileNum) {
 				case 1: 
 					while(scanner.hasNextInt())
 					{
-						source1[i] = scanner.nextInt();
+						source1[i][0] = scanner.nextInt();
+						source1[i][1] = x; 
 						i++;
+						x++;
+						
 					}
 					break;
 				case 2:
 					while(scanner.hasNextInt())
 					{
-						source2[i] = scanner.nextInt();
+						source2[i][0] = scanner.nextInt();
+						source2[i][1] = x; 
 						i++;
+						x++;
 					}
 					break;
 				case 3:
 					while(scanner.hasNextInt())
 					{
-						source3[i] = scanner.nextInt();
+						source3[i][0] = scanner.nextInt();
+						source3[i][1] = x; 
 						i++;
+						x++;
 					}
 					break;
 				case 4:
 					while(scanner.hasNextInt())
 					{
-						source4[i] = scanner.nextInt();
+						source4[i][0] = scanner.nextInt();
+						source4[i][1] = x; 
 						i++;
+						x++;
 					}
 					break;
 				case 5:
 					while(scanner.hasNextInt())
 					{
-						source5[i] = scanner.nextInt();
+						source5[i][0] = scanner.nextInt();
+						source5[i][1] = x; 
 						i++;
+						x++;
 					}
 					break;
 				}
@@ -88,27 +101,28 @@ public class InversionCounter {
 			ee.printStackTrace();}
 	
 	getSum();			//Get sum of each source ranking and create a new array with the data of Summation 
-	quickSort(sum, 0, sum.length - 1);
+	sortedSum = sum;
+	quickSort(sum, 0, source1.length - 1);
 	inversionCounter(source1);
+	reOrder();
 	
 	
 	
 	for (int i = 0; i < 10000; i++)
 	{
-		System.out.println(source1[i]);
+		
+		System.out.print("D: " + source1[i][0]);
+		//System.out.print(" ========  ");
+		System.out.println(" I: " + source1[i][1]);
+		//System.out.println(source1[2][0]);
+		//System.out.println(source1[2][1]);
+		
+		
+		
 	}
 	
-	System.out.println(inversionCount);
-
-	
+	System.out.println("Inversions" + inversionCount);
 }
-	
-	
-	
-	
-	
-	
-	
 //-------------------------------------------------------------------------------------
 	public static void main(String[] args) {
 
@@ -122,7 +136,8 @@ public class InversionCounter {
 	{
 		for (int i = 0; i < 10000; i++)
 		{
-			sum[i] = source1[i] + source2[i] + source3[i] + source4[i] + source5[i];
+			sum[i][0] = source1[i][0] + source2[i][0] + source3[i][0] + source4[i][0] + source5[i][0];
+			sum[i][1] = i;
 		}
 	}
 //-------------------------------------------------------------------------------------
@@ -132,29 +147,53 @@ public class InversionCounter {
 		
 	}
 	
-	int getIndex(int arr[], int left, int right)
+	public void reOrder()
+	{
+		
+		for (int x = 0; x < 10000; x++)
+		{
+			int i = 0;
+			while (source1[i][1] != sum[x][1]) //while not equal, search source file for num
+			{
+				i++;
+				{
+					int tmp = source1[i][1];	//tmp = found index location
+					source1[i][1] = x;			//source index location swapped with correct location 
+					source1[x][1] = tmp;		//location moved
+				}
+			}
+		
+		}
+	}
+	
+	int getIndex(int arr[][], int left, int right)
 
 	{
 	      int i = left;
 	      int j = right;
 	      int tmp;
-	      int pivot = arr[(left + right) / 2]; 	//choose pivot based on middle element 
+	      int pivot = arr[(left + right) / 2][0]; 	//choose pivot based on middle element 
 	      
 	      while (i <= j)  						//While left less then right
 	      {
-	            while (arr[i] < pivot)	
+	            while (arr[i][0] < pivot)	
 	            {								//Increment array to find correct location for data
 	                  i++;
 	            }					
-	            while (arr[j] > pivot)	
+	            while (arr[j][0] > pivot)	
 	            {
 	                  j--;
 	            }
 	            if (i <= j) 
 	            {								//Swap positions of data 
-	                  tmp = arr[i];
-	                  arr[i] = arr[j];
-	                  arr[j] = tmp;
+	                  tmp = arr[i][0];
+	                  arr[i][0] = arr[j][0];
+	                  arr[j][0] = tmp;
+	                  
+	                  tmp = arr[i][1];			//Swap positions of index 
+	                  arr[i][1] = arr[j][1];
+	                  arr[j][1] = tmp;
+	                  
 	                  i++;
 	                  j--;
 	            }
@@ -162,7 +201,7 @@ public class InversionCounter {
 	      return i; //return index 
 	}
 
-	void quickSort(int arr[], int left, int right) //QuickSort an Array with start and end index param 
+	void quickSort(int arr[][], int left, int right) //QuickSort an Array with start and end index param 
 	{
 	      int index = getIndex(arr, left, right);
 	      
@@ -174,19 +213,17 @@ public class InversionCounter {
 
 	}
 	
-	void inversionCounter(int a[]) {
+	void inversionCounter(int a[][]) {
 		  int i, j, temp;
 
 		  for (i = 0; i < a.length-1; i++)
 		  {
 		    for (j = i + 1; j < a.length; j++)
 		    {
-		    	if (a[i] > a[j])
+		    	if (a[i][0] > a[j][0])
 		    	{
 		    		inversionCount++;
-		    		//temp = a[j];
-		    		//a[j] = a[i];
-		    		//a[i] = temp; 
+		    		
 		    	}
 		    }
 		  }
